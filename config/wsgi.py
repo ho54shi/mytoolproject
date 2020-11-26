1c3dd9c6978d8712b83f3ad0db52c02c58c30ca8
ho54shi <hoshikome5772@outlook.jp>
2020-11-26 14:52:57 +0900

    Bacic認証もどき

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from wsgi_basic_auth import BasicAuth # 追加 
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('WSGI_AUTH_CREDENTIALS', '[ID]:[PW]')
 
 application = get_wsgi_application()
+
+application = BasicAuth(application)
